597c31e6bdd1d4222b386d879e2ae0c0919716b9
Shubham270797 <94452849+Shubham270797@users.noreply.github.com>
2022-03-08 07:56:06 +0530

    Add files via upload

---
+++ Advance-JAVA/Hibernate/GroceryProduct.java
@@ -0,0 +1,96 @@
+package study.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="grocery_product")
+public class GroceryProduct {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="product_id")
+	private int productId;
+	
+	@Column(name="product_name")
+	 private String productName;
+	 
+	@Column(name="product_des")
+	private String productDesc;
+	
+	@Column(name="product_unit")
+	private int unit;
+	
+	@Column(name="product_ycost")
+	private int productCost;
+	
+	@Column(name="product_zexpiry_date")
+	private java.sql.Date expiryDate;
+	
+	public GroceryProduct() {
+
+System.out.println("Grocery Entity");
+	}
+	public GroceryProduct( String productName, String productDesc, int unit, int productCost,
+			java.sql.Date expiryDate) {
+		super();
+		/*this.productId = productId;*/
+		this.productName = productName;
+		this.productDesc = productDesc;
+		this.unit = unit;
+		this.productCost = productCost;
+		this.expiryDate = expiryDate;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		 System.out.println("set product id"); 
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		 System.out.println("set product name"); 
+		this.productName = productName;
+	}
+	public String getProductDesc() {
+		return productDesc;
+	}
+	public void setProductDesc(String productDesc) {
+		 System.out.println("set product desc"); 
+		this.productDesc = productDesc;
+	}
+	public int getUnit() {
+		return unit;
+	}
+	public void setUnit(int unit) {
+		 System.out.println("set product unit"); 
+		this.unit = unit;
+	}
+	public int getProductCost() {
+		return productCost;
+	}
+	public void setProductCost(int productCost) {
+		 System.out.println("set product cost"); 
+		this.productCost = productCost;
+	}
+	public java.sql.Date getExpiryDate() {
+		return expiryDate;
+	}
+	public void setExpiryDate(java.sql.Date expiryDate) {
+		 System.out.println("set expiry date"); 
+		this.expiryDate = expiryDate;
+	}
+	@Override
+    public String toString()
+    {
+    	String str=" ";
+    	str=str+"Product Id: "+productId+" Product Name: "+productName+" Product Desc: "+productDesc+" Quantity: "+unit+" Product Cost: "+productCost+" Product Expiry Date: "+expiryDate;
+       return str;
+    }
+	
+	
+}
